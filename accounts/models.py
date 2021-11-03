f3526aa8a023d75ad35e5e0cc371cb57fbf3ef2d
hiwa2719 <hiahmadyan@gmail.com>
2021-11-03 22:13:18 +0330

    implemented User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField()
+    username = models.CharField(max_length=128)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_admin(self):
+        return self.is_superuser
