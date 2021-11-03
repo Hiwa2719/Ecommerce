647850e95c46e12a64f070e4903c6419461d819b
hiwa2719 <hiahmadyan@gmail.com>
2021-11-03 23:07:07 +0330

    implemented UserManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,32 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager as BaseUserManager
 from django.db import models
 
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError
+        if not password:
+            raise ValueError
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        if not extra_fields.setdefault('is_staff', True):
+            raise ValueError
+        if not extra_fields.setdefault('is_superuser', True):
+            raise ValueError
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField()
@@ -13,6 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
     def __str__(self):
         return self.email
 
