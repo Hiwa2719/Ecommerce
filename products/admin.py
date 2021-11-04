61756266ece4fb9c96718626500354312b0c8217
hiwa2719 <hiahmadyan@gmail.com>
2021-11-04 19:28:49 +0330

    added ProductModelAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'is_active', 'is_digital'
+    list_filter = 'is_active', 'is_digital', 'last_update', 'created_date'
+
+    add_fieldset = [
+        (None, {
+            'fields': ('name', 'is_active', 'is_digital')
+        })
+    ]
+
+    def get_fieldsets(self, request, obj=None):
+        if obj:
+            return self.add_fieldset
+        return super().get_fieldsets(request, obj)
