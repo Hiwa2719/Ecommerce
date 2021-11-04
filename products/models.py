62110f08ea5cae1bba06cc341034c9f8bdb3ce7d
hiwa2719 <hiahmadyan@gmail.com>
2021-11-04 15:46:56 +0330

    added ProductAbstractBaseModel

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductAbstractBaseModel(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = unique_slug_generator(self.name)
+        super().save(*args, **kwargs)
