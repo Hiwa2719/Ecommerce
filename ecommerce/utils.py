9bdc76f229cf112b76ac05d34bdf5ab39951be6b
hiwa2719 <hiahmadyan@gmail.com>
2021-11-04 15:58:52 +0330

    implemented unique_slug_generator & unique_string_generator

--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -1,6 +1,9 @@
+import random
+import string
 from datetime import datetime
 
 from django.contrib import messages
+from django.utils.text import slugify
 from django.utils.translation import ngettext
 
 
@@ -38,3 +41,19 @@ def get_past_dates(now=None, years=0, months=0, days=0):
     except ValueError:
         now = datetime(year=year, month=month, day=1)
         return get_past_dates(now, days=-(day - 2))
+
+
+def unique_string_generator(number_of_chars=7, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(number_of_chars))
+
+
+def unique_slug_generator(instance, new_slug=None, number_of_chars=4):
+    if new_slug:
+        slug = new_slug
+    else:
+        slug = slugify(instance.name)
+
+    if instance.__class__.objects.filter(slug=slug).exists():
+        new_slug = f'{slug}-{unique_string_generator()}'
+        return unique_slug_generator(instance, new_slug=new_slug, number_of_chars=number_of_chars)
+    return slug
