0b22787cbf7beea520ba0de47ba15e42003e33c3
hiwa2719 <hiahmadyan@gmail.com>
2021-11-12 20:29:15 +0330

    added Comment Model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.BooleanField()
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.content[:50]
