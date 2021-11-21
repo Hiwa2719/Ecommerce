ba20f131ceb96c7941bec4be3bca00553f188287
Hiwa2719 <56973950+Hiwa2719@users.noreply.github.com>
2021-11-21 17:24:41 +0330

    added CommentModelAdmin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = 'user', 'product', 'positivity', 'publish_date'
+    list_filter = 'publish_date',
+    search_fields = 'product__name', 'content'
+
+    @admin.display(description='recommend')
+    def positivity(self, obj):
+        if obj.rating == 'pos':
+            return 'Yes'
+        return 'No'
--- comments/models.py
+++ comments/models.py
@@ -1,15 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 User = get_user_model()
 
 
 class Comment(models.Model):
+    RATING_CHOICES = [
+        ('pos', _('I recommend buying this product.')),
+        ('neg', _('I don\'t recommend buying this product.')),
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.BooleanField()
+    rating = models.CharField(max_length=3, choices=RATING_CHOICES)
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
-    publish_date = models.DateTimeField()
+    publish_date = models.DateTimeField(blank=True, null=True)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
 
     def __str__(self):
