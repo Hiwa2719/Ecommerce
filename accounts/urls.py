d155bff50be49a7cbcf37d0d20e097d7fbf56195
Hiwa2719 <56973950+Hiwa2719@users.noreply.github.com>
2021-11-22 13:02:15 +0330

    added few views to accounts app and profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,9 @@ app_name = 'accounts'
 urlpatterns = [
                   path('profile/', accounts_views.ProfileView.as_view(), name='profile'),
                   path('signup/', accounts_views.SignupView.as_view(), name='signup'),
+                  path('delete-account/', accounts_views.DeleteAccountView.as_view(), name='delete_account'),
+                  path('delete-success/', accounts_views.DeleteSuccessView.as_view(), name='delete_success'),
+                  path('ordre-list/', accounts_views.OrderListView.as_view(), name='order-list'),
                   path('password_change/',
                        views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')),
                        name='password_change'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, DeleteView, ListView
+
 from .forms import SignupForm
 
 User = get_user_model()
@@ -17,3 +18,20 @@ class SignupView(CreateView):
     form_class = SignupForm
     model = User
     success_url = reverse_lazy('accounts:login')
+
+
+class DeleteAccountView(LoginRequiredMixin, DeleteView):
+    success_url = reverse_lazy('accounts:delete_success')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class DeleteSuccessView(TemplateView):
+    template_name = 'accounts/delete_success.html'
+
+
+class OrderListView(ListView):
+    # todo implement this view with Order model
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
