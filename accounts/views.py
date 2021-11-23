ac1d9b485afeafece0c40e851bc42096cd89ec4a
Hiwa2719 <56973950+Hiwa2719@users.noreply.github.com>
2021-11-23 22:02:11 +0330

    added EmailVerification view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.http import Http404
+from django.template import loader
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, DeleteView, ListView
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.views.generic import CreateView, TemplateView, DeleteView, ListView, View
 
 from .forms import SignupForm
 from .models import Customer
@@ -19,6 +26,30 @@ class SignupView(CreateView):
     form_class = SignupForm
     model = Customer
     success_url = reverse_lazy('accounts:login')
+
+
+class EmailVerification(View):
+    def get(self, request, *args, **kwargs):
+        customer = request.user.customer
+        if not customer.verified_email:
+            current_site = get_current_site(request)
+            context = {
+                'email': customer.email,
+                'domain': current_site.domain,
+                'site_name': current_site.name,
+                'uid': urlsafe_base64_encode(force_bytes(customer.id)),
+                'user': customer,
+                'token': default_token_generator.make_token(customer),
+                'protocol': 'http'
+            }
+            body = loader.render_to_string('accounts/verification_email_template.html', context)
+            send_mail(
+                subject=f'Email verification at {current_site.name}',
+                message=body,
+                from_email='hiahmadyan@gmail.com',
+                recipient_list=[customer.email, ]
+            )
+        raise Http404
 
 
 class DeleteAccountView(LoginRequiredMixin, DeleteView):
