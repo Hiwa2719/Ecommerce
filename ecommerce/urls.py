d7ae74fe9b9a5c25c7f9b90cc3b385fcc04b1519
Hiwa2719 <56973950+Hiwa2719@users.noreply.github.com>
2021-11-24 21:29:41 +0330

    added ProductDetailView and it's url and template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
